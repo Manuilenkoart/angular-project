f605eed072e556cef496c4c1741afb4f4d3c1497
manuilenkoart <manuilenkoart@gmail.com>
2021-03-01 23:13:34 +0200

    update contentComponent to angular materia sstyles

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -6,19 +6,60 @@ export interface CurrencyInterface{
   sale: number;
   buy: number;
 }
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
 })
+
+
+// export class ContentComponent implements OnInit {
+//   getCurrencyFromApi: CurrencyInterface[] = [];
+//   constructor(private http: HttpClient) { }
+//   date: Date = new Date();
+//   ngOnInit(): void {
+//     this.http.get<CurrencyInterface[]>('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5').subscribe(res => {
+//       this.getCurrencyFromApi = res;
+//     });
+//   }
+//
+// }
+
+
+
 export class ContentComponent implements OnInit {
-  getCurrencyFromApi: CurrencyInterface[] = [];
-  constructor(private http: HttpClient) { }
   date: Date = new Date();
-  ngOnInit(): void {
+  dataSource: CurrencyInterface[] = [];
+
+
+  displayedColumns: string[] = ['ccy', 'buy', 'sale'];
+  // dataSource = ELEMENT_DATA;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
     this.http.get<CurrencyInterface[]>('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5').subscribe(res => {
-      this.getCurrencyFromApi = res;
+      this.dataSource = res;
     });
   }
 
 }
+
+
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
-const Material = [MatDialogModule, MatInputModule, MatButtonModule];
+const Material = [MatDialogModule, MatInputModule, MatButtonModule, MatTableModule];
 
 @NgModule({
   imports: [Material],
