f6dbb70ff6bcaf5ae55b7ff1fab68825ce610b95
manuilenkoart <manuilenkoart@gmail.com>
2021-03-03 10:54:32 +0200

    refactoring header link button to angular material styled button

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,8 +3,9 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
 
-const Material = [MatDialogModule, MatInputModule, MatButtonModule, MatTableModule];
+const Material = [MatDialogModule, MatInputModule, MatButtonModule, MatTableModule, MatCardModule];
 
 @NgModule({
   imports: [Material],
